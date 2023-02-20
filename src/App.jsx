361d634552cb9868e8df44f98eacb93e85ea6438
gustavoale <gustavo.1@hotmail.com.ar>
2023-02-20 17:49:44 -0300

    3 commit

--- src/App.jsx
+++ src/App.jsx
@@ -3,19 +3,27 @@ import NavBar from './components/NavBar';
 import Carousel  from "./components/Carousel";
 import Destacados from './components/Destacados';
 import Recomendados from './components/Recomendados';
-import ItemListContainer from './components/ItemListContainer';
+import Promo from './components/Promo';
 import Footer from './components/Footer';
+import ItemListContainer from './components/ItemListContainer';
+import ItemDetailContainer from './components/ItemDetailContainer';
+import {BrowserRouter, Routes, Route} from "react-router-dom"
 
 const App = () => {
   return (
-    <div>
-      <NavBar/>
-      <Carousel/>
-      <Recomendados/>
-      <Destacados/>
-      <ItemListContainer promo={{mensaje:"Recibí todas nuestras ofertas",registro:"¿Querés recibir nuestras ofertas? ¡Registrate ahora y comenza a recibirlas!"}}/>
-      <Footer/>
-    </div>
+    <BrowserRouter>
+      <div>
+        <NavBar/>
+        <Routes>
+          <Route path={"/"} element={<div> <Carousel/> <Recomendados/> <Destacados/> <ItemListContainer/> </div>}/>
+          <Route path={"/category/:id"} element={ <div><ItemListContainer/></div> }/>
+          <Route path={`/item/:id`} element={<ItemDetailContainer/>}/>
+        </Routes>
+        <Promo/>
+        <Footer/>
+      </div>
+    </BrowserRouter>
+    
   );
 }
 
--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,5 +1,6 @@
 import CardWidget from './CardWidget'
 import logo from './images/logo.png'
+import {Link, NavLink} from 'react-router-dom'
 
 const NavBar = () => {
     return (
@@ -9,8 +10,8 @@ const NavBar = () => {
                     <button className="navbar-toggler bg-white" type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvasNavbar" aria-controls="offcanvasNavbar">
                     <span className="navbar-toggler-icon "></span>
                     </button>
-                    <a className="navbar-brand text-light position-absolute top-50
-                     start-50 translate-middle" href={"/"} ><img src={logo} width={190} alt="LOGO" height={68} /> </a>
+                    <Link className="navbar-brand text-light position-absolute top-50
+                     start-50 translate-middle" to={"/"} ><img src={logo} width={190} alt="LOGO" height={68} /> </Link>
                     <div className="offcanvas bg-black offcanvas-start" tabindex="-1" id="offcanvasNavbar" aria-labelledby="offcanvasNavbarLabel">
                         <div className="offcanvas-header">
                             <h5 className="offcanvas-title text-light" id="offcanvasNavbarLabel"><img src={logo} alt="LOGO" width={250} height={65} /></h5>
@@ -18,23 +19,83 @@ const NavBar = () => {
                         </div>
                         <div className="offcanvas-body">
                             <ul className="navbar-nav justify-content-end flex-grow-1 pe-3">
-                                <li className="nav-item">
-                                    <a className="nav-link active text-light" aria-current="page" href={"/"}>Home</a>
-                                </li>
-                                <li className="nav-item">
-                                    <a className="nav-link text-light" href={"/"}>Link</a>
+                                <li className="nav-item dropdown bg-black">
+                                    <li  className="nav-link dropdown-toggle  text-light" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                                    Procesadores
+                                    </li>
+                                    <ul className="dropdown-menu bg-black">
+                                        <li><NavLink className="nav-link active  ms-3 text-secondary" to={"/category/Procesadores"}>-Ver todo en Procesadores</NavLink></li>
+                                        <li><NavLink className="nav-link active  ms-3 text-secondary" to={"/"}>-Procesadores AMD</NavLink></li>
+                                        <li><NavLink className="nav-link active ms-3 text-secondary" to={"/"}>-Procesadores Intel</NavLink></li>
+                                        
+                                    </ul>
                                 </li>
                                 <li className="nav-item dropdown bg-black">
-                                    <a className="nav-link dropdown-toggle  text-light" href={"/"} role="button" data-bs-toggle="dropdown" aria-expanded="false">
-                                    Dropdown
-                                    </a>
+                                    <NavLink className="nav-link dropdown-toggle  text-light" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                                    Mothers
+                                    </NavLink>
                                     <ul className="dropdown-menu bg-black">
-                                        <li><a className="dropdown-item text-light" href={"/"}>Action</a></li>
-                                        <li><a className="dropdown-item text-light" href={"/"}>Another action</a></li>
-                                        <li>
-                                            <hr className="dropdown-divider"/>
-                                        </li>
-                                        <li><a className="dropdown-item text-light" href={"/"}>Something else here</a></li>
+                                        <li><NavLink className="nav-link active  ms-3 text-secondary" to={"/category/Mothers"}>-Ver todo en Mothers</NavLink></li>
+                                        <li><NavLink className="nav-link active  ms-3 text-secondary" to={"/"}>-Mothers AMD</NavLink></li>
+                                        <li><NavLink className="nav-link active ms-3 text-secondary" to={"/"}> Intel</NavLink></li>
+                                        
+                                    </ul>
+                                </li>
+                                <li className="nav-item dropdown bg-black">
+                                    <NavLink className="nav-link dropdown-toggle  text-light" to={"/"} role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                                    Placas de Video
+                                    </NavLink>
+                                    <ul className="dropdown-menu bg-black">
+                                        <NavLink className="nav-link ms-3 text-secondary" to={"/category/Placas_de_video"} activeClassName={"active"}>-Ver todo en Placas de Video</NavLink>
+                                        <li><NavLink className="nav-link active  ms-3 text-secondary" to={"/"}>-Placas de Video GeForce</NavLink></li>
+                                        <li><NavLink className="nav-link active ms-3 text-secondary" to={"/"}>-Placas de Video Radeon AMD</NavLink></li>
+                                        
+                                    </ul>
+                                </li>
+                                <li className="nav-item dropdown bg-black">
+                                    <NavLink className="nav-link dropdown-toggle  text-light" to={"/"} role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                                    Memorias RAM
+                                    </NavLink>
+                                    <ul className="dropdown-menu bg-black">
+                                        <li><NavLink className="nav-link active  ms-3 text-secondary" to={"/category/Memorias_RAM"}>-Ver todo en Memorias RAM</NavLink></li>
+                                        <li><NavLink className="nav-link active  ms-3 text-secondary" to={"/"}>-Memorias</NavLink></li>
+                                        <li><NavLink className="nav-link active ms-3 text-secondary" to={"/"}>-Memorias Notebook</NavLink></li>
+                                    </ul>
+                                </li>
+                                <li className="nav-item dropdown bg-black">
+                                    <NavLink className="nav-link dropdown-toggle  text-light" to={"/"} role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                                    Refrigeración
+                                    </NavLink>
+                                    <ul className="dropdown-menu bg-black">
+                                        <li><NavLink className="nav-link active  ms-3 text-secondary" to={"/category/Refrigeracion"}>-Ver todo en Refrigeracion</NavLink></li>
+                                        <li><NavLink className="nav-link active  ms-3 text-secondary" to={"/"}>-Coolers Fan</NavLink></li>
+                                        <li><NavLink className="nav-link active ms-3 text-secondary" to={"/"}>-Coolers CPU</NavLink></li>
+                                        <li><NavLink className="nav-link active ms-3 text-secondary" to={"/"}>-Pasta Termica</NavLink></li>
+                                    </ul>
+                                </li>
+                                <li className="nav-item dropdown bg-black">
+                                    <NavLink className="nav-link dropdown-toggle  text-light" to={"/"} role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                                    Gabinetes
+                                    </NavLink>
+                                    <ul className="dropdown-menu bg-black">
+                                        <li><NavLink className="nav-link active  ms-3 text-secondary" to={"/category/Gabinetes"}>-Gabinetes</NavLink></li>
+                                    </ul>
+                                </li>
+                                <li className="nav-item dropdown bg-black">
+                                    <NavLink className="nav-link dropdown-toggle  text-light" to={"/"} role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                                    Fuentes
+                                    </NavLink>
+                                    <ul className="dropdown-menu bg-black">
+                                        <li><NavLink className="nav-link active  ms-3 text-secondary" to={"/category/Fuentes"}>-Fuentes de Alimentación</NavLink></li>
+                                    </ul>
+                                </li>
+                                <li className="nav-item dropdown bg-black">
+                                    <NavLink className="nav-link dropdown-toggle  text-light" to={"/category/Sillas_Gamers"} role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                                    Sillas Gamers
+                                    </NavLink>
+                                    <ul className="dropdown-menu bg-black">
+                                        <li><NavLink className="nav-link active  ms-3 text-secondary" to={"/category/Sillas_Gamers"}>-Sillas Gamers</NavLink></li>
+                                        
                                     </ul>
                                 </li>
                             </ul>
