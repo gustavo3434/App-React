729725f01221f971ec4512d1e6724456a4f2fe1a
gustavoale <gustavo.1@hotmail.com.ar>
2023-03-19 20:13:29 -0300

    5 commit

--- src/components/ItemDetailContainer.jsx
+++ src/components/ItemDetailContainer.jsx
@@ -2,20 +2,17 @@ import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import arrayProductos from "./json/data.json"
 import ItemDetail from "./ItemDetail";
+import {doc, collection, getDoc, getFirestore } from "firebase/firestore";
 
 const ItemDetailContainer = () => {
     const [items, setItems] = useState ([])
     const {id} = useParams();
 
     useEffect (() => {
-        const promesa = new Promise ((resolve) => {
-            setTimeout(() => {
-                resolve(arrayProductos.find(item => item.id === parseInt(id)))
-            },2000)
-        })
-        promesa.then((respuesta) => {
-            setItems(respuesta)
-        })
+        const db = getFirestore()
+        const itemsCollection = doc(db,"productos",id)
+        getDoc(itemsCollection).then(element => {setItems({id:element.id,... element.data()})})
+       
     },[id])
 
     return(
