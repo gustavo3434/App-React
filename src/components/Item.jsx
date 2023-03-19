c2fe4802976e326b2df1af35fc20e88490ac4206
gustavoale <gustavo.1@hotmail.com.ar>
2023-03-19 20:15:37 -0300

    5 commit

--- src/components/Item.jsx
+++ src/components/Item.jsx
@@ -7,7 +7,6 @@ const Item = ({item}) => {
             <section className="card-body">
             <NavLink to={`/item/${item.id}`} className="text-decoration-none text-dark"><h5 className="card-title ">{item.nombre}</h5></NavLink>
                 <h6>${item.precio}</h6>
-                <button type="button" className="btn btn-dark position-absolute bottom-0 start-50 translate-middle-x mb-2">Agregar</button>
             </section>
         </div>
     )
