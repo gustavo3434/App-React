a9af9207d6c9f321ee9b8da3d23ee39f6957ff11
gustavoale <gustavo.1@hotmail.com.ar>
2023-03-27 12:05:48 -0300

    7 commit

--- src/components/Formulario.jsx
+++ src/components/Formulario.jsx
@@ -34,23 +34,24 @@ const Formulario = () => {
     }
 
     return(
-        <div>
+        <div className="">
             <h3 className="text-light text-center text-bold" style={{marginTop: 140}}> Formulario de Compra</h3>
             <form action="" className="mt-5">
-                <div style={{marginLeft: "-5%"}}>
-                    <label htmlFor="nombre" className="form-label">Nombre y apellido</label>
-                    <input type="text" className="form-cantrol" id="nombre" placeholder="Juan Perez(ejemplo)" onInput={(e) => {setNomApe(e.target.value)}} style={{width:"25%"}}/>
+                <div className="mt-3 ms-5">
+                    <input type="text" className="form-cantrol" id="nombre" placeholder="Juan Perez(ejemplo)" onInput={(e) => {setNomApe(e.target.value)}} style={{width:"30%"}}/>
                 </div>
-                <div className="mt-3" style={{marginLeft: "-5%"}}>
-                    <label htmlFor="email" className="form-label">Nombre y apellido</label>
-                    <input type="text" className="form-cantrol" id="email" placeholder="alguien@gmail.com(ejemplo)" onInput={(e) => {setEmail(e.target.value)}} style={{width:"25%"}}/>
+                <div className="mt-3 ms-5" >
+                    <input type="text" className="form-cantrol" id="email" placeholder="alguien@gmail.com(ejemplo)" onInput={(e) => {setEmail(e.target.value)}} style={{width:"30%"}}/>
                 </div>
-                <div className="mt-3" style={{marginLeft: "-5%"}}>
-                    <label htmlFor="telefono" className="form-label">Nombre y apellido</label>
-                    <input type="text" className="form-cantrol" id="telefono" placeholder="1123456789(ejemplo)" onInput={(e) => {setTelefono(e.target.value)}} style={{width:"25%"}}/>
+                <div className="mt-3 ms-5" >
+                    <input type="text" className="form-cantrol" id="telefono" placeholder="1123456789(ejemplo)" onInput={(e) => {setTelefono(e.target.value)}} style={{width:"30%"}}/>
                 </div>
             </form>
-            <button className="btn mt-5 position-absolute bottom-0 start-50 translate-middle-x" style={{backgroundColor: "green"}} onClick={checkCamposComplet(nomApe,email,telefono) ? generarOrden : ""}>Generar Orden</button>
+            
+            <div className="mt-5 d-flex justify-content-center">
+                <button className="btn " style={{backgroundColor: "green"}} onClick={checkCamposComplet(nomApe,email,telefono) ? generarOrden : ""}>Generar Orden</button>
+            </div>
+            
             {orderId? <div className="alert alert-success" role="alert" style={{marginLeft:"25%" ,width: "50%"}}> {nomApe.toUpperCase()} Se genero una Orden de compra, ¡ MUCHAS GRACIAS POR ELEGIRNOS ¡ </div>: ""}
             <div style={{marginTop:90}}>a</div>
         </div>
